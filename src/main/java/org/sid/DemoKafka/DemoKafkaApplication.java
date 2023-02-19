ba42205bbaee14fe372602f44c86eb09b6cf5b08
ayoub_bsl <bsl.ayoub20@gmail.com>
2023-02-19 23:03:24 +0100

    first commit

---
+++ src/main/java/org/sid/DemoKafka/DemoKafkaApplication.java
@@ -0,0 +1,14 @@
+package org.sid.DemoKafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoKafkaApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(DemoKafkaApplication.class, args);
+	}
+
+}
